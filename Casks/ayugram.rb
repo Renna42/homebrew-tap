41d80cdfabc37c4aacdff7562442adb2df603bd1
Renna Z. <is@renna.dev>
2026-03-05 21:57:59 +0800

    initialization of this repository with 3 casks

---
+++ Casks/ayugram.rb
@@ -0,0 +1,16 @@
+cask "ayugram" do
+  version "6.3.10"
+  sha256 "9ac1c4f4e7ca32fa0fdc07006639d17fffe346de59898dfe0a217d588879c62a"
+
+  url "https://github.com/AyuGram/AyuGramDesktop/releases/download/v#{version}/AyuGram.dmg"
+  name "AyuGram"
+  desc "Telegram client with ghost mode and message history"
+  homepage "https://github.com/AyuGram/AyuGramDesktop"
+
+  app "AyuGram.app"
+
+  zap trash: [
+    "~/Library/Application Support/AyuGram Desktop",
+    "~/Library/Saved Application State/one.ayugram.AyuGramDesktop.savedState",
+  ]
+end
---
+++ Casks/dbeaver.rb
@@ -0,0 +1,21 @@
+cask "dbeaver" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "26.0.0"
+  sha256 arm:   "5b31d585a6dc2179e87625c1ff97c50aef7b61c3e26c1e8ddb59bfed3f3f6aaa",
+         intel: "1b55efe27b9c6f4b828dba12d6bbed7f2eab8b4a073c7666bbe8de6bd3c2e90e"
+
+  depends_on macos: ">= :big_sur"
+
+  url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg",
+      verified: "github.com/dbeaver/dbeaver/"
+  name "DBeaver"
+  desc "Free Open-Source Database Management Tool"
+  homepage "https://dbeaver.io"
+
+  app "DBeaver.app"
+
+  zap trash: [
+    "~/Library/DBeaverData/",
+  ]
+end
