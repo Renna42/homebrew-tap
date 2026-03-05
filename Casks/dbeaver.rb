2abcc31fd48308cb64175a60b6f84f09dede3317
Renna Z. <is@renna.dev>
2026-03-05 23:01:18 +0800

    applying brew style

--- Casks/dbeaver.rb
+++ Casks/dbeaver.rb
@@ -5,17 +5,15 @@ cask "dbeaver" do
   sha256 arm:   "5b31d585a6dc2179e87625c1ff97c50aef7b61c3e26c1e8ddb59bfed3f3f6aaa",
          intel: "1b55efe27b9c6f4b828dba12d6bbed7f2eab8b4a073c7666bbe8de6bd3c2e90e"
 
-  depends_on macos: ">= :big_sur"
-
   url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos-#{arch}.dmg",
       verified: "github.com/dbeaver/dbeaver/"
   name "DBeaver"
   desc "Free Open-Source Database Management Tool"
   homepage "https://dbeaver.io/"
 
+  depends_on macos: ">= :big_sur"
+
   app "DBeaver.app"
 
-  zap trash: [
-    "~/Library/DBeaverData/",
-  ]
+  zap trash: "~/Library/DBeaverData/"
 end
